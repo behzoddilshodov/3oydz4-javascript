4fea54de13b5595c0cec6b5d69766aefbd5d5103
behzoddilshodov <behzodpubg@gmail.com>
2022-07-03 01:13:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var userName = "Farruhbek";
+// console.log(userName);
+// var userAge = 17;
+// console.log(userAge);
+
+alert("Siz rezyume topshirishka rozimisiz?")
+
+var promptInput = prompt("USername", "Kimdir");
+var promptSurname = prompt("Surname", "Palonchiyev");
+var promptWork = prompt("What do you do?", "kimdir bolib ishlayman");
+var promptAge = prompt("Age", "16");
+var promptHobby = prompt("Hobbies", "Basketball");
+var promptPhone = prompt("your phone model", "Iphone");
+
+alert(`Name: ${promptInput}, \nSurname:${promptSurname}, \nWork: ${promptWork}, \nAge: ${promptAge}, \nHobbies: ${promptHobby}, \nPhone model: ${promptPhone}`)
+
+console.log(`Name: ${promptInput}, \nSurname:${promptSurname}, \nWork: ${promptWork}, \nAge: ${promptAge}, \nHobbies: ${promptHobby}, \nPhone model: ${promptPhone}`);
+
